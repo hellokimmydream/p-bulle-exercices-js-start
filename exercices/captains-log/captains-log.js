20478b40936ec033cc152c4d27056c286e08e21d
CamilleJulieRais <rais.camille@gmail.com>
2025-11-28 16:00:47 +0100

    feat[exercice terminé]
    50min

--- exercices/captains-log/captains-log.js
+++ exercices/captains-log/captains-log.js
@@ -6,9 +6,13 @@
  * @returns {string} the generated registry number.
  */
 export function randomShipRegistryNumber() {
-  throw new Error(
-    'Please remove this line and implement the randomShipRegistryNumber() function',
-  );
+
+    //math.floor = entier entre 0 et 8999 + 1000 = entier entre 1000 et 9000
+    // math.random * 9000 parce que je veux un nombre entre 0 et 9000
+const number = Math.floor(Math.random() * 9000) + 1000;
+
+// backtick pour insérer une variable
+return `NCC-${number}`;
 }
 
 /**
@@ -17,9 +21,8 @@ export function randomShipRegistryNumber() {
  * @returns {number} a stardate between 41000 (inclusive) and 42000 (exclusive).
  */
 export function randomStardate() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+
+  return Math.random() * 1000 + 41000;
 }
 
 /**
@@ -28,7 +31,12 @@ export function randomStardate() {
  * @returns {string} a one-letter planet class.
  */
 export function randomPlanetClass() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+
+  // en maj = constante fixe jamais modifié
+  const CLASSES = 'DHJKLMNRTY' ;
+
+  // .length = nbr entre 0 et length
+  const index = Math.floor(Math.random() * CLASSES.length);
+
+  return CLASSES[index];
 }
