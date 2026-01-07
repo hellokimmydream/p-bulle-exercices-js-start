5675fec781b8de3f95e89b8674e37903fb889ea3
CamilleJulieRais <rais.camille@gmail.com>
2026-01-07 13:40:04 +0100

    feat(exercice terminé)
    rattrapage de mixed juice fait a la maison le 28.12

--- exercices/mixed-juices/mixed-juices.js
+++ exercices/mixed-juices/mixed-juices.js
@@ -1,38 +1,51 @@
 // @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
 
-/**
- * Determines how long it takes to prepare a certain juice.
- *
- * @param {string} name
- * @returns {number} time in minutes
- */
 export function timeToMixJuice(name) {
-  throw new Error('Remove this line and implement the function');
+  switch (name) {
+    case "Pure Strawberry Joy":
+      return 0.5;
+    case "Energizer":
+    case "Green Garden":
+      return 1.5;
+    case "Tropical Island":
+      return 3;
+    case "All or Nothing":
+      return 5;
+    default:
+      return 2.5;
+  }
 }
 
-/**
- * Calculates the number of limes that need to be cut
- * to reach a certain supply.
- *
- * @param {number} wedgesNeeded
- * @param {string[]} limes
- * @returns {number} number of limes cut
- */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Remove this line and implement the function');
+  if (wedgesNeeded <= 0) return 0;
+
+  let wedges = 0;
+  let cut = 0;
+
+  const wedgeBySize = {
+    small: 6,
+    medium: 8,
+    large: 10,
+  };
+
+  while (cut < limes.length && wedges < wedgesNeeded) {
+    wedges += wedgeBySize[limes[cut]] ?? 0;
+    cut++;
+  }
+
+  return cut;
 }
 
-/**
- * Determines which juices still need to be prepared after the end of the shift.
- *
- * @param {number} timeLeft
- * @param {string[]} orders
- * @returns {string[]} remaining orders after the time is up
- */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Remove this line and implement the function');
+  let time = timeLeft;
+  let index = 0;
+
+  // tant qu'il reste du tps on commence une commande
+  while (index < orders.length && time > 0) {
+    time -= timeToMixJuice(orders[index]);
+    index++;
+  }
+
+  // renvoit les commandes non commencées
+  return orders.slice(index);
 }
