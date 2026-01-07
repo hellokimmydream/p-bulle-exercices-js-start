164b9a1ced8e1841bff232554df94ad6e9867875
CamilleJulieRais <rais.camille@gmail.com>
2026-01-07 13:50:29 +0100

    feat(exercice terminé)
    rattrapage a la maison de elysee analytic enchantement le 29.12

--- exercices/elyses-analytic-enchantments/enchantments.js
+++ exercices/elyses-analytic-enchantments/enchantments.js
@@ -1,73 +1,25 @@
 // @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
 
-/**
- * Get the position (index) of the card in the given stack
- *
- * @param {number[]} stack
- * @param {number} card
- *
- * @returns {number} position of the card in the stack
- */
 export function getCardPosition(stack, card) {
-  throw new Error('Implement the getCardPosition function');
+  return stack.indexOf(card);
 }
 
-/**
- * Determine if the stack contains the card
- *
- * @param {number[]} stack
- * @param {number} card
- *
- * @returns {boolean} true if card is in the stack, false otherwise
- */
 export function doesStackIncludeCard(stack, card) {
-  throw new Error('Implement the doesStackIncludeCard function');
+  return stack.includes(card);
 }
 
-/**
- * Determine if each card is even
- *
- * @param {number[]} stack
- *
- * @returns {boolean} true if all cards are even, false otherwise
- */
 export function isEachCardEven(stack) {
-  throw new Error('Implement the isEachCardEven function');
+  return stack.every((value) => value % 2 === 0);
 }
 
-/**
- * Check if stack contains odd-value card
- *
- * @param {number[]} stack
- *
- * @returns {boolean} true if the array contains odd card, false otherwise
- */
 export function doesStackIncludeOddCard(stack) {
-  throw new Error('Implement the doesStackIncludesOddCard function');
+  return stack.some((value) => value % 2 !== 0);
 }
 
-/**
- * Get the first odd card from the stack
- *
- * @param {number[]} stack
- *
- * @returns {number | undefined} the first odd value
- */
 export function getFirstOddCard(stack) {
-  throw new Error('Implement the getFirstOddCard function');
+  return stack.find((value) => value % 2 !== 0);
 }
 
-/**
- * Determine the position of the first card that is even
- *
- * @param {number[]} stack
- *
- * @returns {number} position of the first card that is even
- */
 export function getFirstEvenCardPosition(stack) {
-  throw new Error('Implement the getFirstEvenCardPosition function');
+  return stack.findIndex((value) => value % 2 === 0);
 }
