e6b7484084ba665da210c9ff590fe22f3c41eace
CamilleJulieRais <rais.camille@gmail.com>
2026-01-09 15:18:54 +0100

    feat(train-driver)
    [50][Done] travail avec ...

--- exercices/train-driver/train-driver.js
+++ exercices/train-driver/train-driver.js
@@ -10,48 +10,32 @@
  * @param {...number} ids
  * @returns {number[]} wagon ids
  */
-export function getListOfWagons(a, b, c, d, e, f, g, h, i, j, k, l, m, n) {
-  throw new Error('Remove this line and implement the function');
+export function getListOfWagons(...ids) {
+  return ids;
 }
 
-/**
- * Reorder the array of wagons by moving the first 2 wagons to the end of the array.
- *
- * @param {Iterable<number>} ids
- * @returns {number[]} reordered list of wagons
- */
-export function fixListOfWagons(ids) {
-  throw new Error('Remove this line and implement the function');
+// avec ...qqch on récupére/copie ce qu il y a dedans
+
+// ...rest récupére le reste
+export function fixListOfWagons([firstWagon, secondWagon, ...rest]) {
+  // ... fais une copie crée retourne nouveau tableau
+  // ... c est un tableau qui contient les élément du tableau original
+  // ... pour copier ou décomposer un tableau en un nouveau tableau élément par élément, ne modifie pas original,
+  // ...rest sépare les élément
+  return [...rest, firstWagon, secondWagon];
 }
 
-/**
- * Fixes the array of wagons by inserting an array of wagons after the first element in eachWagonsID.
- *
- * @param {Iterable<number>} ids
- * @param {Iterable<number>} missingWagons
- * @returns {number[]} corrected list of wagons
- */
-export function correctListOfWagons(ids, missingWagons) {
-  throw new Error('Remove this line and implement the function');
+export function correctListOfWagons([locomotiveId, ...rest], missingWagons) {
+  // met tout dans 1 tableau dans l'ordre voulu
+  return [locomotiveId, ...missingWagons, ...rest];
 }
 
-/**
- * Extend route information by adding another object
- *
- * @param {Record<string, string>} information
- * @param {Record<string, string>} additional
- * @returns {Record<string, string>} extended route information
- */
 export function extendRouteInformation(information, additional) {
-  throw new Error('Remove this line and implement the function');
+  // Fusionne 2 objet et onleve les doublons
+  return { ...information, ...additional };
 }
 
-/**
- * Separate arrival time from the route information object
- *
- * @param {Record<string, string>} information
- * @returns {[string, Record<string, string>]} array with arrival time and object without arrival time
- */
-export function separateTimeOfArrival(information) {
-  throw new Error('Remove this line and implement the function');
+export function separateTimeOfArrival({ timeOfArrival, ...rest }) {
+  // extrait timeOfArriv + reste et copie dans une nouveau tableau
+  return [timeOfArrival, rest];
 }
